7a2bf36308352e90ca5f261e2144ea2f5ecc0f45
Bill St. Clair <billstclair@gmail.com>
2026-09-22 12:32:35 -0400

    bin/build

--- src/WebXR.elm
+++ src/WebXR.elm
@@ -6,7 +6,7 @@
 -- Copyright (c) 2026 Bill St. Clair <billstclair@gmail.com>
 -- Some rights reserved.
 -- Distributed under the MIT License
--- See LICENSE.txt
+-- See LICENSE
 --
 ----------------------------------------------------------------------
 
